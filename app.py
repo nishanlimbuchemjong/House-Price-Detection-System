220d2b912fdd82db5c7088cc6b4cc4193c1c53d6
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-02-03 17:22:20 +0545

    [modified] home and predict_api function created that predict the house price

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pickle
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
+# load the model:
+house_price_model = pickle.load(open('house_price_model.pkl', 'rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
 
-
-
-
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/predict_api', method=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = house_price_model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
